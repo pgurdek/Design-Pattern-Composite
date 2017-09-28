ba0b7e8089a901d2ce11813dd00769f1f041dd9f
Piotr <pgurdek@gmail.com>
2017-09-28 15:07:14 +0200

    Add init structure of project

---
+++ src/menu/Menu.java
@@ -0,0 +1,17 @@
+package menu;
+
+
+import java.util.Iterator;
+
+public class Menu extends MenuComponent {
+	
+	public Menu(String name, String url) {
+		this.name = name;
+		this.url = url;
+	}
+
+	@Override
+	public String toString() {
+		return null;
+	}
+}
---
+++ src/menu/MenuComponent.java
@@ -0,0 +1,23 @@
+package menu;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class MenuComponent {
+	
+	String name;
+	String url;
+	List<MenuComponent> menuComponents = new ArrayList<>();
+
+	public String getName() {
+		return name;
+	}
+	
+	public String getUrl() {
+		return url;
+	}	
+	
+	public abstract String toString();
+
+}
